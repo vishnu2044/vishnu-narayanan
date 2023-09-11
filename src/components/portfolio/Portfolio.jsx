7f083c8bf8a841bd66265daad9a6c16ec9221f81
vishnu narayanan <vishnunarayanan2044@gmail.com>
2023-09-11 14:29:05 +0530

    services, portfolio completed

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,85 @@
 import React from 'react'
 import "./portfolio.css";
+import BookStall from "../../assets/works/bookstall_django_project.png";
+import MsiStatic from "../../assets/works/MSI_static.png";
+import MsiRespo from "../../assets/works/msi_responsive.png";
+import OGHealthCare from "../../assets/works/og_healthcare.png";
 
 const Portfolio = () => {
   return (
     <div>
-      <section id='portfolio'> Portfolio</section>
+      <section id='portfolio'>
+        <h5>My recent work</h5>
+        <h2>Portfolio</h2>
+
+        <div className='container portfolio__container'>
+
+          <article className='protfolio__item'>
+            <div className='portfolio__item-image'>
+              <img src={BookStall} alt="" />
+            </div>
+            <h3>BookStall Ecom website (django)</h3>
+            <div className="portfolio__item-cta">
+              <a href="https://github.com/vishnu2044/BookStall" className='btn'>GitHub</a>
+              <a href="https://bookstall.store/" className='btn btn-primary' target='_blank' >live demo</a>
+            </div>
+
+          </article>
+
+          <article className='protfolio__item'>
+            <div className='portfolio__item-image'>
+              <img src={MsiRespo} alt="" />
+            </div>
+            <h3>MSI website front page (responsive)</h3>
+            <div className="portfolio__item-cta">
+              <a href="https://github.com/vishnu2044/msi-clone" className='btn'>GitHub</a>
+              <a href="https://vishnu2044.github.io/msi-clone/" className='btn btn-primary' target='_blank' >live demo</a>
+            </div>
+          </article>
+
+          <article className='protfolio__item'>
+            <div className='portfolio__item-image'>
+              <img src={MsiStatic} alt="" />
+            </div>
+            <h3>Msi front page</h3>
+            <div className="portfolio__item-cta">
+              <a href="https://github.com/vishnu2044/msi-webpage-front-page" className='btn'>GitHub</a>
+              <a href="https://vishnu2044.github.io/msi-webpage-front-page/" className='btn btn-primary' target='_blank' >live demo</a>
+            </div>
+          </article>
+
+          <article className='protfolio__item'>
+            <div className='portfolio__item-image'>
+              <img src={OGHealthCare} alt="" />
+            </div>
+            <h3>OG HealthCare front page </h3>
+            <div className="portfolio__item-cta">
+              <a href="https://github.com/vishnu2044/GG-healthcare-front-page" className='btn'>GitHub</a>
+              <a href="https://vishnu2044.github.io/GG-healthcare-front-page/" className='btn btn-primary' target='_blank' >live demo</a>
+            </div>
+          </article>
+
+          {/* <article className='protfolio__item'>
+            <div className='portfolio__item-image'>
+              <img src={BookStall} alt="" />
+            </div>
+            <h3>BookStall Ecom website (django)</h3>
+            <a href="https://github.com/vishnu2044/BookStall" className='btn'>GitHub</a>
+            <a href="https://bookstall.store/" className='btn btn-primary' target='_blank' >live demo</a>
+          </article>
+
+          <article className='protfolio__item'>
+            <div className='portfolio__item-image'>
+              <img src={MsiStatic} alt="" />
+            </div>
+            <h3>Msi front page</h3>
+            <a href="https://github.com/vishnu2044/msi-webpage-front-page" className='btn'>GitHub</a>
+            <a href="https://vishnu2044.github.io/msi-webpage-front-page/" className='btn btn-primary' target='_blank' >live demo</a>
+          </article> */}
+
+        </div>
+
+      </section>
     </div>
   )
 }
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,10 +1,91 @@
 import React from 'react'
 import "./services.css";
+import {BsCheckLg} from 'react-icons/bs';
 
 const Services = () => {
   return (
     <div>
-      <section id='services'>Services</section>
+      <section id='services'>
+        <h5>What is offer</h5>
+        <h2>Services</h2>
+        <div className='container services__container'>
+          <article className='service'>
+            <div className='service__head'>
+              <h3>Frontend Development</h3>
+            </div>
+            <ul className='service__list'>
+              <li>
+                <BsCheckLg className='service__list-icon'/>
+                <p>Custom HTML and CSS Development with React Integration</p>
+              </li>
+              <li>
+                <BsCheckLg className='service__list-icon'/>
+                <p>Interactive React Solutions</p>
+              </li>
+              <li>
+                <BsCheckLg className='service__list-icon'/>
+                <p>API Integration with React</p>
+              </li>
+              <li>
+                <BsCheckLg className='service__list-icon'/>
+                <p>TResponsive Web Design with React Components</p>
+              </li>
+            </ul>
+            {/* END OF FRONT END TAGE */}
+          </article>
+
+          <article className='service'>
+            <div className='service__head'>
+              <h3>Backend Development</h3>
+            </div>
+            <ul className='service__list'>
+              <li>
+                <BsCheckLg className='service__list-icon'/>
+                <p>Custom Django Application Development</p>
+              </li>
+              <li>
+                <BsCheckLg className='service__list-icon'/>
+                <p> Design and Management</p>
+              </li>
+              <li>
+                <BsCheckLg className='service__list-icon'/>
+                <p>API Development</p>
+              </li>
+              <li>
+                <BsCheckLg className='service__list-icon'/>
+                <p>Authentication and Authorization</p>
+              </li>
+              <li>
+                <BsCheckLg className='service__list-icon'/>
+                <p>Integration of Third-Party Services</p>
+              </li>
+              <li>
+                <BsCheckLg className='service__list-icon'/>
+                <p>Maintenance and Support</p>
+              </li>
+            </ul>
+            {/* END OF BACK END TAGE */}
+          </article>
+
+          <article className='service'>
+            <div className='service__head'>
+              <h3>Database</h3>
+            </div>
+            <ul className='service__list'>
+              <li>
+                <BsCheckLg className='service__list-icon'/>
+                <p>Database Design and Optimization</p>
+              </li>
+              <li>
+                <BsCheckLg className='service__list-icon'/>
+                <p>Data Modeling and Documentation</p>
+              </li>
+            </ul>
+            {/* END OF FRONT END TAGE */}
+          </article>
+        </div>
+
+      </section>
     </div>
   )
 }
