fcdec492067f54b1e421eb1e474384fdf8f74f33
vishnu narayanan <vishnunarayanan2044@gmail.com>
2024-02-14 13:42:29 +0530

    added linkedIn profile

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -7,7 +7,7 @@ const HeaderSocials = () => {
   return (
     <div className='header__socials'>
         <a href="https://www.linkedin.com/in/vishnu-narayanan2044/" target='_blank'> <BsLinkedin /> </a>
-        <a href="https://www.github.com/" target='_blank'><FaGithubSquare /></a>
+        <a href="https://github.com/vishnu2044" target='_blank'><FaGithubSquare /></a>
         <a href="https://www.instagram.com/" target='_blank'> <BsInstagram /> </a>
 
     </div>
