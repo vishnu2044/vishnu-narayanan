61dd1bdb084504b232133152058b7d81e9c4d297
vishnu narayanan <vishnunarayanan2044@gmail.com>
2024-02-28 14:20:45 +0530

    added new resume

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import CV from "../../assets/cv.pdf";
+import resume from '../../assets/Django-React-Developer-Vishnu Narayanan.pdf';
 
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={CV} download className='btn' >Download CV</a>
+        <a href={resume} download className='btn' >Download CV</a>
         <a href='#contact' className='btn btn-primary'>Lets Talk</a>
         
     </div>
